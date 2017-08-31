bc6c2d4a7b688c99ee974f5e3ac34aa4dfea0175
BastiRenken <sebastian-renken@t-online.de>
2017-08-31 11:29:26 +0200

    Update

--- bmirechner.py
+++ bmirechner.py
@@ -1,4 +1,6 @@
 # Eingabe
+#!/usr/bin/python3
+
 print("Haben sie ihr Idealgewicht?")
 masse = float(input("Ihr Gewicht in kg: "))
 groesse = float(input("Ihre Größe in m: "))
@@ -12,5 +14,7 @@ if bmi < 18.5:
     print("Sie haben untergewicht.")
 if 18.5 <= bmi <= 25:
     print("Sie haben Normalgewicht.")
-if bmi > 25:
+if 25 < bmi <= 30:
     print("Sie haben Übergewicht.")
+if bmi > 30:
+    print("Sie leiden unter Adipositas.")
