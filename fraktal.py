5b158d5370afb12dba6966bd171d3895a6e409fc
BastiRenken <sebastian-renken@t-online.de>
2017-08-31 12:05:55 +0200

    New file

---
+++ fraktal.py
@@ -0,0 +1,26 @@
+from turtle import *
+import time
+
+STOP = 2
+START = 200
+
+def strecke(n):
+    if n <= STOP:
+        fd(n)
+    else:
+        strecke(n/3)
+        left(60)
+        strecke(n/3)
+        right(120)
+        strecke(n/3)
+        left(60)
+        strecke(n/3)
+
+speed(0)
+left(60)
+for i in range(3):
+    strecke(START)
+    right(120)
+
+hideturtle()
+time.sleep(30)
