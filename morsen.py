7280c9d1ae4953c721044efae7fe3d794500e451
BastiRenken <sebastian-renken@t-online.de>
2017-09-02 10:53:29 +0200

    Update

--- morsen.py
+++ morsen.py
@@ -9,14 +9,14 @@ zeichenliste = [('a', '.-'), ('b', '-...'), ('c', '-.-.'), ('d', '-..'),
 ('4', '....-'), ('5', '.....'), ('6', '-....'), ('7', '--...'), ('8', '---..'),
 ('9', '----.'), ('0', '-----'), (' ', '/'), ('.', '.-.-.-'), (',', '--..--'),
 (':', '---...'), ('?', '..--..'), ('-', '-....-'), ('/', '-..-.'),
-('+', '.-.-.'), ('=', '-...-'), ('(', '-.--.-'), ('@', '.--.-.')]
+('+', '.-.-.'), ('=', '-...-'), ('(', '-.--.'), (')', '-.--.-'), ('@', '.--.-.')]
 
 status = 1
 
 print("Willst du:")
-print("-(T)ext in Morsecode umwandeln?")
+print("-[T]ext in Morsecode umwandeln?")
 print("oder")
-print("-(M)orsecode in Text umwandeln?")
+print("-[M]orsecode in Text umwandeln?")
 
 while status == 1:
     antwort = input(" ")
@@ -33,7 +33,8 @@ while status == 1:
                     morsecode.extend(zeichenliste[j][1])
                     morsecode.extend(" ")
                     break
-        print("".join(morsecode))
+        ausgabe = ("".join(morsecode))
+        print("Morsecode: ", ausgabe)
         status = 0
 
     elif antwort == "m":
@@ -45,8 +46,21 @@ while status == 1:
                 if morsecode[i] == zeichenliste[j][1]:
                     text.extend(zeichenliste[j][0])
                     break
-        print("".join(text))
+        ausgabe = ("".join(text))
+        print("Text: ", ausgabe)
         status = 0
 
     else:
         print("Geben sie T oder M ein und bestätigen sie mit Enter.")
+
+print("Willst du das Ergebnis speichern?")
+print("[J]a oder [N]ein")
+
+'''
+auswahl = input(" ")
+auswahl = auswahl.lower()
+if auswahl == "J":
+    ergebnis = open("/Users/basti/Desktop/morsecode.txt", "a")
+    ergebnis.write(ausgabe)
+    ergebnis.close()
+'''
