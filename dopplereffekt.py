cd336fa7f4d35f200e4ef069443afd2fc8ebf2cf
BastiRenken <sebastian-renken@t-online.de>
2017-09-05 21:32:08 +0200

    Update

--- dopplereffekt.py
+++ dopplereffekt.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
-
 # Anleitung
 print("")
 print("Dieses Programm berechnet die Verändereung der Frequenz auf Grund des Dopplereffekts.")
-print("Angegeben werden müssen:")
-print("- Frequenz des ursprünglichen Tons")
-print("- Geschwindigkeit der Schallquelle")
-print("")
-
 # Lichtgeschwindigkeit in m/s
 c = 334.0
-
+# Eingabe
 status = "j"
 while status == "j":
-    # Eingabe
     f = input("Frequenz in Hz: ")
     f = float(f)
     v = input("Geschwindigkeit in m/s: ")
     v = float(v)
-
-    print(
-        "Bewegt sich die Schallquelle vom Beobachter [w]eg oder zu ihm [h]er")
+    print("Bewegt sich die Schallquelle vom Beobachter [w]eg oder zu ihm [h]er")
     richtung = input("").lower()
     while richtung != "w" and richtung != "h":
         print("Geben sie w oder h ein.")
         richtung = input("").lower()
-
 # Berechnung
-    if richtung == "w":
-        scheinfrequenz = f * (c / (c - v))
-    elif richtung == "h":
-        scheinfrequenz = f * (c / (c + v))
-
-    print("Die registrierte Frequenz beträgt %f Hz." % scheinfrequenz)
-    print("")
-
-    print("Nochmal? [J]a oder [N]ein")
+    if v >= c:
+        print("Überschallknall")
+    else:
+        if richtung == "w":
+            scheinfrequenz = f * (c / (c - v))
+        elif richtung == "h":
+            scheinfrequenz = f * (c / (c + v))
+# Ausgabe
+        print("Die registrierte Frequenz beträgt %f Hz." % scheinfrequenz)
+        print("")
+# Wiederholung
+    print("Nochmal? [J]a [N]ein")
     status = input("").lower()
     while status != "j" and status != "n":
         print("Geben Sie J oder N ein.")
