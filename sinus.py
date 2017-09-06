0794c462a96b40241604f820aa747e128f25da96
BastiRenken <sebastian-renken@t-online.de>
2017-09-06 16:08:50 +0200

    Update

--- sinus.py
+++ sinus.py
@@ -2,10 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 x = np.arange(0.0, 1.0, 0.01)
 
+frequenz1 = 440/440
+frequenz2 = 440/453.58
+
+
+
 fig = plt.figure(1)
 
 ax1 = fig.add_subplot(211)
-ax1.plot(x, np.sin(2*2*np.pi*x))
+ax1.plot(x, np.sin(frequenz1*16*np.pi*x))
 ax1.grid(True)
 ax1.set_ylim((-2, 2))
 ax1.set_title("Sinuskurven")
@@ -13,9 +18,8 @@ ax1.set_title("Sinuskurven")
 for label in ax1.get_xticklabels():
     label.set_color('r')
 
-
 ax2 = fig.add_subplot(212)
-ax2.plot(x, np.sin(2*2*np.pi*x))
+ax2.plot(x, np.sin(frequenz2*16*np.pi*x))
 ax2.grid(True)
 ax2.set_ylim((-2, 2))
 
